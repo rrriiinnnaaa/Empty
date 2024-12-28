44d9b9f5c8eac373b680353ad8cd8101c329799e
Arina <arinalikesnevra@gmail.com>
2024-12-28 15:25:44 +0900

    Delete function

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,14 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi():
-    # Use a breakpoint in the code line below to debug your script.
-    userName = input("Введите имя: ")
-    print(f'Hello, {userName} ')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi()
+    userName = input("Введите имя: ")
+    print(f'Hello, {userName} ')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
