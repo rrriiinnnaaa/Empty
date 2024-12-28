08094b8214bf7b9ed0af9ff2e323a518397c9994
Arina <arinalikesnevra@gmail.com>
2024-12-28 15:12:21 +0900

    User name request

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,13 +4,14 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'{name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    userName = input("Введите имя: ")
+    print(f'Hello, {userName} ')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hello world!')
+    print_hi()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
